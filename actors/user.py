e4f6b4aa6253c9ec9f4a39c935e09d8f4a30055f
fabiola-espinoza-castellon <54430728+fabiola-espinoza-castellon@users.noreply.github.com>
2023-01-24 15:15:06 +0100

    Delete actors directory

--- actors/user.py
+++
@@ -1,9 +0,0 @@
-class User():
-    def __init__(self, idx, train_indexes, test_indexes):
-        self.idx = idx
-        self.train_indexes = train_indexes
-        self.test_indexes = test_indexes
-        self.attacked = False
-        self.nk = len(train_indexes)
-        self.clean_acc = []
-        self.poise_acc = []
